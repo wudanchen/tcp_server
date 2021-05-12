139800154fedf39cfd6406718d93b25eb8a07c64
wudancheng <wudancheng@maker-ray.com>
2021-05-12 20:33:32 +0800

    [feature] 上传ch1

---
+++ ch1/server.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handling(char *message);
+
+int main(int agrc, char *argv[])
+{
+    fb = socket()
+}
